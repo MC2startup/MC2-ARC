2742e5f2167867f2be6dca2970acd6fbab31d81b
dotcom900825 <dotcom900825@gmail.com>
2013-09-02 22:31:08 +0800

    devise gem 用户登录、注册功能及相关页面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Mc2::Application.routes.draw do
+  devise_for :users
+
   resources :posts
   root :to => 'home#index'
 
@@ -51,8 +53,6 @@ Mc2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
