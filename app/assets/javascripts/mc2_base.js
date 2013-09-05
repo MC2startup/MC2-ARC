5cf8d873e9424af7407789b21f401698d7e0b5e8
dotcom900825 <dotcom900825@gmail.com>
2013-09-05 13:05:20 +0800

    重构js代码

--- app/assets/javascripts/mc2_base.js
+++ app/assets/javascripts/mc2_base.js
@@ -1,20 +1,12 @@
 (function() {
-  $(window).scroll(function() {
+  /*$(window).scroll(function() {
     if ($(this).scrollTop() > 10){
       $(".navbar ul.nav, .read-more").fadeOut(200);
     }
     else{
       $(".navbar ul.nav, .read-more").show(200);
     }
-  });
+  }); */
 
-$(".navbar .container").hover(
-  function(){
-    $(".navbar ul.nav").show();
-  },function(){
-    if ($(window).scrollTop() != 0){
-      $(".navbar ul.nav").hide();
-    }
-  });
 
 })();
